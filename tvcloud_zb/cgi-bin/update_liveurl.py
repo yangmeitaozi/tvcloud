cdd07b86f5326eb5f42a8aec99f64cf9e6b44a90
peng <yangmeitaozi@sina.com>
2014-09-15 09:32:30 +0800

    add tvcloud_zb again

--- tvcloud_zb/cgi-bin/update_liveurl.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python
-#coding:utf-8
-import mysql
-
-def update_liveurl():
-    
-    sqlmess = """insert into chmsdb.live_url (chid,live_path) select sortnumber,bsstype from epgdb_live.publish_catalog where catalogcode='channel' """
